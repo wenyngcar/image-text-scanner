3aebf0878a529bac7b216f553bdd97e26b617fdd
wenyngcar <owenyoungcarino@gmail.com>
2025-08-15 19:12:37 +0800

    fix: conflicting style names on primary

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,6 +1,6 @@
 function Footer() {
   return (
-    <div className="text-center border-t border-primary p-5 mt-24">
+    <div className="text-center border-t border-blue-primary p-5 mt-24">
       © 2025 Wordsnap · Owen Cariño
     </div>
   );
