201eebb691aa9a7595e0fef54fad82f6743326c6
wenyngcar <owenyoungcarino@gmail.com>
2025-08-15 18:35:59 +0800

    style: scale button variants border radius to rounded-3xl

--- frontend/src/components/UploadImage.tsx
+++ frontend/src/components/UploadImage.tsx
@@ -3,21 +3,21 @@ import { Button } from "./ui/button"
 
 function UploadImage() {
   return (
-    <>
+    <div className="space-y-5">
       <div className="bg-primary-gradient-reverse rounded-lg h-52 p-0.5 outline-5 -outline-offset-3 outline-dashed outline-primary-bg">
         <div className="bg-primary-bg rounded-md h-full w-full p-5 flex flex-col justify-center items-center gap-5">
           <div className="flex flex-col items-center">
             <img src={uploadIcon} alt="Upload" />
             <div>Upload image here</div>
           </div>
-          <Button className="w-full">Upload Image</Button>
+          <Button size={"lg"} className="w-full text-lg">Upload Image</Button>
         </div>
       </div>
       <div className="flex flex-col space-y-4">
-        <Button>Translate Text</Button>
-        <Button>Reset</Button>
+        <Button size={"lg"} className="text-lg">Translate Text</Button>
+        <Button size={"lg"} className="text-lg">Reset</Button>
       </div>
-    </>
+    </div>
   )
 }
 
