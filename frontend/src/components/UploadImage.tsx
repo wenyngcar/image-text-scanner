4a93e0be90d6c6c97f748ecb8e4a909fc4199b57
wenyngcar <owenyoungcarino@gmail.com>
2025-08-16 10:36:26 +0800

    feat: initialize preview image

--- frontend/src/components/UploadImage.tsx
+++ frontend/src/components/UploadImage.tsx
@@ -21,33 +21,41 @@ function UploadImage() {
   }
 
   return (
-    <div className="space-y-5 md:flex md:space-y-0 md:items-center md:space-x-10">
-      <div className="bg-primary-gradient-reverse rounded-lg h-52 p-0.5 outline-5 -outline-offset-3 outline-dashed outline-primary-bg md:basis-2/3">
-        <div className="bg-primary-bg rounded-md h-full w-full p-5 flex flex-col justify-center items-center gap-5">
-          <div className="flex flex-col items-center space-y-1">
-            <img src={uploadIcon} alt="Upload" />
-            <div>Upload image here</div>
-          </div>
-          {/* For custom file input button. */}
-          <input type="file" className="hidden" ref={inputRef} onChange={(e) => inputOnChange(e)} />
-          <div className="flex flex-col items-center space-y-1">
-            {/* File name will display here. */}
-            <div>{file?.name}</div>
-            <Button size={"lg"} className="text-lg" onClick={uploadImage}>
-              Upload Image
-            </Button>
-          </div>
+    <>
+      <div className="hidden md:block h-96 w-full bg-primary-gradient p-0.5 rounded-lg">
+        <div className="bg-primary-bg h-full w-full rounded-md text-center p-2">
+          <div className="text-2xl font-semibold">Preview Uploaded Image</div>
+          <div className="flex flex-col h-[93%] justify-center text-3xl">No Image Uploaded</div>
         </div>
       </div>
-      <div className="flex flex-col space-y-4 bg-red-300 md:basis-1/3">
-        <Button size={"lg"} className="text-lg">
-          Translate Text
-        </Button>
-        <Button size={"lg"} className="text-lg" onClick={handleReset}>
-          Reset
-        </Button>
+      <div className="space-y-5 md:flex md:space-y-0 md:items-center md:space-x-10">
+        <div className="bg-primary-gradient-reverse rounded-lg h-52 p-0.5 outline-5 -outline-offset-3 outline-dashed outline-primary-bg md:basis-2/3">
+          <div className="bg-primary-bg rounded-md h-full w-full p-5 flex flex-col justify-center items-center gap-5">
+            <div className="flex flex-col items-center space-y-1">
+              <img src={uploadIcon} alt="Upload" />
+              <div>Upload image here</div>
+            </div>
+            {/* For custom file input button. */}
+            <input type="file" className="hidden" ref={inputRef} onChange={(e) => inputOnChange(e)} />
+            <div className="flex flex-col items-center space-y-1">
+              {/* File name will display here. */}
+              <div>{file?.name}</div>
+              <Button size={"lg"} className="text-lg" onClick={uploadImage}>
+                Upload Image
+              </Button>
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-col space-y-4 md:basis-1/3">
+          <Button size={"lg"} className="text-lg">
+            Translate Text
+          </Button>
+          <Button size={"lg"} className="text-lg" onClick={handleReset}>
+            Reset
+          </Button>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
