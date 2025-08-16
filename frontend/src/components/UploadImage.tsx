0032ec803dc0f33ea46bdcf79e771bb2e294dc3e
wenyngcar <owenyoungcarino@gmail.com>
2025-08-16 11:28:07 +0800

    format: npm run format

--- frontend/src/components/UploadImage.tsx
+++ frontend/src/components/UploadImage.tsx
@@ -3,28 +3,28 @@ import { Button } from "./ui/button";
 import { useRef, useState } from "react";
 
 function UploadImage() {
-  const inputRef = useRef<HTMLInputElement>(null)
-  const [file, setFile] = useState<File | null>(null)
-  const [preview, setPreview] = useState<string>()
+  const inputRef = useRef<HTMLInputElement>(null);
+  const [file, setFile] = useState<File | null>(null);
+  const [preview, setPreview] = useState<string>();
 
   // For reference only so that button will prompt file when click.
   const uploadImage = () => {
-    inputRef.current?.click()
-  }
+    inputRef.current?.click();
+  };
 
   const inputOnChange = (e: any) => {
-    setFile(e.target.files[0])
-    setPreview(URL.createObjectURL(e.target.files[0]))
-  }
+    setFile(e.target.files[0]);
+    setPreview(URL.createObjectURL(e.target.files[0]));
+  };
 
   // Removes the image and the translated text.
   function handleReset() {
-    setFile(null)
-    setPreview("")
+    setFile(null);
+    setPreview("");
 
-    // Clear also the reference. 
+    // Clear also the reference.
     if (inputRef.current) {
-      inputRef.current.value = ""
+      inputRef.current.value = "";
     }
   }
 
@@ -48,7 +48,13 @@ function UploadImage() {
               <div>Upload image here</div>
             </div>
             {/* For custom file input button. */}
-            <input type="file" className="hidden" ref={inputRef} onChange={(e) => inputOnChange(e)} accept=".png, .jpg, .jpeg" />
+            <input
+              type="file"
+              className="hidden"
+              ref={inputRef}
+              onChange={(e) => inputOnChange(e)}
+              accept=".png, .jpg, .jpeg"
+            />
             <div className="flex flex-col items-center space-y-1">
               {/* File name will display here. */}
               <div>{file?.name}</div>
@@ -59,7 +65,10 @@ function UploadImage() {
           </div>
         </div>
         <div className="flex flex-col justify-between space-y-2 md:basis-1/3">
-          <div className="text-gray-400">Accepted format: .png, .jpg, .jpeg. If your image is not compatible, please convert them before uploading.</div>
+          <div className="text-gray-400">
+            Accepted format: .png, .jpg, .jpeg. If your image is not compatible,
+            please convert them before uploading.
+          </div>
           <div className="flex flex-col space-y-4">
             <Button size={"lg"} className="text-lg">
               Translate Text
