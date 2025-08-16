a4d5f31a8a6cf5b5f5cc6a4d97f6873c9a568151
wenyngcar <owenyoungcarino@gmail.com>
2025-08-16 09:35:44 +0800

    feat: custom upload image button

--- frontend/src/components/UploadImage.tsx
+++ frontend/src/components/UploadImage.tsx
@@ -1,7 +1,20 @@
 import uploadIcon from "../assets/icons/upload-primary.svg";
 import { Button } from "./ui/button";
+import { useRef, useState } from "react";
 
 function UploadImage() {
+  const inputRef = useRef<HTMLInputElement>(null)
+  const [file, setFile] = useState<File>()
+
+  // For reference only so that button will prompt file when click.
+  const uploadImage = () => {
+    inputRef.current?.click()
+  }
+
+  const inputOnChange = (e: any) => {
+    setFile(e.target.files[0])
+  }
+
   return (
     <div className="space-y-5">
       <div className="bg-primary-gradient-reverse rounded-lg h-52 p-0.5 outline-5 -outline-offset-3 outline-dashed outline-primary-bg">
@@ -10,9 +23,15 @@ function UploadImage() {
             <img src={uploadIcon} alt="Upload" />
             <div>Upload image here</div>
           </div>
-          <Button size={"lg"} className="w-full text-lg">
-            Upload Image
-          </Button>
+          {/* For custom file input button. */}
+          <input type="file" className="hidden" ref={inputRef} onChange={(e) => inputOnChange(e)} />
+          <div className="flex flex-col items-center space-y-1">
+            {/* File name will display here. */}
+            <div>{file?.name}</div>
+            <Button size={"lg"} className="text-lg" onClick={uploadImage}>
+              Upload Image
+            </Button>
+          </div>
         </div>
       </div>
       <div className="flex flex-col space-y-4">
