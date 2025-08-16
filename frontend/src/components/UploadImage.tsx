79d8172fe25f2461e5e5b7e9bdd35669f84850a3
wenyngcar <owenyoungcarino@gmail.com>
2025-08-16 09:58:33 +0800

    feat: handle reset function

--- frontend/src/components/UploadImage.tsx
+++ frontend/src/components/UploadImage.tsx
@@ -4,7 +4,7 @@ import { useRef, useState } from "react";
 
 function UploadImage() {
   const inputRef = useRef<HTMLInputElement>(null)
-  const [file, setFile] = useState<File>()
+  const [file, setFile] = useState<File | null>(null)
 
   // For reference only so that button will prompt file when click.
   const uploadImage = () => {
@@ -15,9 +15,14 @@ function UploadImage() {
     setFile(e.target.files[0])
   }
 
+  // Removes the image and the translated text.
+  function handleReset() {
+    setFile(null)
+  }
+
   return (
-    <div className="space-y-5">
-      <div className="bg-primary-gradient-reverse rounded-lg h-52 p-0.5 outline-5 -outline-offset-3 outline-dashed outline-primary-bg">
+    <div className="space-y-5 md:flex md:space-y-0 md:items-center md:space-x-10">
+      <div className="bg-primary-gradient-reverse rounded-lg h-52 p-0.5 outline-5 -outline-offset-3 outline-dashed outline-primary-bg md:basis-2/3">
         <div className="bg-primary-bg rounded-md h-full w-full p-5 flex flex-col justify-center items-center gap-5">
           <div className="flex flex-col items-center space-y-1">
             <img src={uploadIcon} alt="Upload" />
@@ -34,11 +39,11 @@ function UploadImage() {
           </div>
         </div>
       </div>
-      <div className="flex flex-col space-y-4">
+      <div className="flex flex-col space-y-4 bg-red-300 md:basis-1/3">
         <Button size={"lg"} className="text-lg">
           Translate Text
         </Button>
-        <Button size={"lg"} className="text-lg">
+        <Button size={"lg"} className="text-lg" onClick={handleReset}>
           Reset
         </Button>
       </div>
