69d5d71527f35de0da158bf7168c5d9c43bfbfc6
wenyngcar <owenyoungcarino@gmail.com>
2025-08-17 09:43:55 +0800

    feat: add loading spin animation

--- frontend/src/components/LoadingScreen.tsx
+++ frontend/src/components/LoadingScreen.tsx
@@ -1,6 +1,13 @@
+import spinnerIcon from "../assets/icons/spinner.svg"
+
 function LoadingScreen() {
   return (
-    <div className="fixed inset-0 bg-black/50"></div>
+    <div className="fixed inset-0 bg-black/50">
+      <div className="absolute left-1/2 top-1/2 -translate-1/2 space-y-3">
+        <img src={spinnerIcon} className="animate-spin" />
+        <span> Loading... </span>
+      </div>
+    </div>
   )
 }
 
