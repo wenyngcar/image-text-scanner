d0df303a8bbdbcf9be611d812c6bb0bc9be09328
wenyngcar <owenyoungcarino@gmail.com>
2025-08-18 15:39:12 +0800

    feat: display text and language

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,5 @@
 import Footer from "./components/Footer";
 import Header from "./components/Header";
-import TranslatedText from "./components/TranslatedText";
 import UploadImage from "./components/UploadImage";
 
 function App() {
@@ -8,7 +7,6 @@ function App() {
     <div className="container p-5 space-y-20 xl:p-7 xl:px-16">
       <Header />
       <UploadImage />
-      <TranslatedText />
       <Footer />
     </div>
   );
--- frontend/src/components/UploadImage.tsx
+++ frontend/src/components/UploadImage.tsx
@@ -3,12 +3,15 @@ import { Button } from "./ui/button";
 import { useRef, useState } from "react";
 import LoadingScreen from "./LoadingScreen";
 import api from "@/api/api";
+import TranslatedText from "./TranslatedText";
+import type { detectedText } from "@/types/detectedText";
 
 function UploadImage() {
   const inputRef = useRef<HTMLInputElement>(null);
   const [file, setFile] = useState<File | null>(null);
   const [preview, setPreview] = useState<string>();
   const [loading, setLoading] = useState(false);
+  const [detectedText, setDetectedText] = useState<detectedText>()
 
   // For reference only so that button will prompt file when click.
   const uploadImage = () => {
@@ -31,19 +34,21 @@ function UploadImage() {
     }
   }
 
-  function delay(ms: number) {
-    return new Promise((resolve) => setTimeout(resolve, ms));
-  }
-
   // Function when clicking the translate text button.
   async function handleTranslate() {
     setLoading(true);
-    // Intentionally display the loading screen for 3 seconds for better UI/UX.
+
+    // Wrap in formdata instead of raw file because backend expects formdata.
+    const formData = new FormData()
+    if (file) {
+      // First argument must match the key in the backend.
+      formData.append("image", file)
+    }
 
     try {
-      const res = await api.get("/");
-      console.log(res.data);
-      await delay(3000);
+      const res = await api.post('/upload-image', formData)
+      // console.log(res.data);
+      setDetectedText(res.data)
     } catch (error) {
       console.error(`Error handling data: ${error}`);
     } finally {
@@ -52,58 +57,61 @@ function UploadImage() {
   }
 
   return (
-    <div className="space-y-20 lg:flex lg:space-x-[5%] lg:space-y-0 ">
-      <div className="hidden md:block h-96 w-full bg-primary-gradient p-0.5 rounded-lg xl:h-[500px]">
-        <div className="bg-primary-bg h-full w-full rounded-md text-center p-2 space-y-5">
-          <div className="text-2xl font-semibold">Preview Uploaded Image</div>
-          {preview ? (
-            <img src={preview} className="mx-auto object-contain h-[80%]" />
-          ) : (
-            <div className="text-3xl mt-[20%] ">No Image Uploaded</div>
-          )}
+    <>
+      <div className="space-y-20 lg:flex lg:space-x-[5%] lg:space-y-0 ">
+        <div className="hidden md:block h-96 w-full bg-primary-gradient p-0.5 rounded-lg xl:h-[500px]">
+          <div className="bg-primary-bg h-full w-full rounded-md text-center p-2 space-y-5">
+            <div className="text-2xl font-semibold">Preview Uploaded Image</div>
+            {preview ? (
+              <img src={preview} className="mx-auto object-contain h-[80%]" />
+            ) : (
+              <div className="text-3xl mt-[20%] ">No Image Uploaded</div>
+            )}
+          </div>
         </div>
-      </div>
-      <div className="space-y-5 md:flex md:space-y-0 md:space-x-10 lg:flex-col lg:space-y-5 xl:justify-between ">
-        <div className="bg-primary-gradient-reverse rounded-lg h-52 w-full p-0.5 outline-5 -outline-offset-3 outline-dashed outline-primary-bg md:basis-2/3">
-          <div className="bg-primary-bg rounded-md h-full w-full p-5 flex flex-col justify-center items-center gap-5">
-            <div className="flex flex-col items-center space-y-1">
-              <img src={uploadIcon} alt="Upload" />
-              <div>Upload image here</div>
+        <div className="space-y-5 md:flex md:space-y-0 md:space-x-10 lg:flex-col lg:space-y-5 xl:justify-between ">
+          <div className="bg-primary-gradient-reverse rounded-lg h-52 w-full p-0.5 outline-5 -outline-offset-3 outline-dashed outline-primary-bg md:basis-2/3">
+            <div className="bg-primary-bg rounded-md h-full w-full p-5 flex flex-col justify-center items-center gap-5">
+              <div className="flex flex-col items-center space-y-1">
+                <img src={uploadIcon} alt="Upload" />
+                <div>Upload image here</div>
+              </div>
+              {/* For custom file input button. */}
+              <input
+                type="file"
+                className="hidden"
+                ref={inputRef}
+                onChange={(e) => inputOnChange(e)}
+                accept=".png, .jpg, .jpeg"
+              />
+              <div className="flex flex-col items-center space-y-1">
+                {/* File name will display here. */}
+                <div>{file?.name}</div>
+                <Button size={"lg"} className="text-lg" onClick={uploadImage}>
+                  Upload Image
+                </Button>
+              </div>
             </div>
-            {/* For custom file input button. */}
-            <input
-              type="file"
-              className="hidden"
-              ref={inputRef}
-              onChange={(e) => inputOnChange(e)}
-              accept=".png, .jpg, .jpeg"
-            />
-            <div className="flex flex-col items-center space-y-1">
-              {/* File name will display here. */}
-              <div>{file?.name}</div>
-              <Button size={"lg"} className="text-lg" onClick={uploadImage}>
-                Upload Image
+          </div>
+          <div className="flex flex-col justify-between space-y-2 md:basis-1/3">
+            <div className="text-gray-400">
+              Accepted format: .png, .jpg, .jpeg. If your image is not compatible,
+              please convert them before uploading.
+            </div>
+            <div className="flex flex-col space-y-4">
+              <Button size={"lg"} className="text-lg" onClick={handleTranslate}>
+                Translate Text
+                {loading && <LoadingScreen />}
+              </Button>
+              <Button size={"lg"} className="text-lg" onClick={handleReset}>
+                Reset
               </Button>
             </div>
           </div>
         </div>
-        <div className="flex flex-col justify-between space-y-2 md:basis-1/3">
-          <div className="text-gray-400">
-            Accepted format: .png, .jpg, .jpeg. If your image is not compatible,
-            please convert them before uploading.
-          </div>
-          <div className="flex flex-col space-y-4">
-            <Button size={"lg"} className="text-lg" onClick={handleTranslate}>
-              Translate Text
-              {loading && <LoadingScreen />}
-            </Button>
-            <Button size={"lg"} className="text-lg" onClick={handleReset}>
-              Reset
-            </Button>
-          </div>
-        </div>
       </div>
-    </div>
+      <TranslatedText language={detectedText?.language} text={detectedText?.text} />
+    </>
   );
 }
 
