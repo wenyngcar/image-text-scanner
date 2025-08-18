766dc42a7eaee8933c81d7e6b3cc72b813083e9d
wenyngcar <owenyoungcarino@gmail.com>
2025-08-18 11:14:04 +0800

    feat: initialize basic endpoint

---
+++ backend/main.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+# URL of the frontend.
+# Replace if deployed.
+origins = [
+    "http://localhost:5173"
+]
+
+# Allows the send and receive of request from the specified port(origins).
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*']
+)
+
+
+@app.get('/')
+def read_root():
+    return {"message": "Hello World"}
