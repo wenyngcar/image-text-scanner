7b3bc15d5aa06c994050daddef936a35f47fd947
wenyngcar <owenyoungcarino@gmail.com>
2025-08-19 16:09:52 +0800

    fix: translated text minimum height

--- frontend/src/components/TranslatedText.tsx
+++ frontend/src/components/TranslatedText.tsx
@@ -3,7 +3,7 @@ import type { detectedText } from "@/types/detectedText";
 function TranslatedText({ language, text }: detectedText) {
   return (
     <div className="bg-primary-gradient-reverse rounded-lg min-h-52 p-0.5">
-      <div className="bg-primary-bg rounded-md h-full w-full p-5 space-y-2">
+      <div className="bg-primary-bg rounded-md min-h-52 h-full w-full p-5 space-y-2">
         <div className="text-2xl">
           {language ? `Detected Language: ${language}` : "Detected Language:"}
         </div>
