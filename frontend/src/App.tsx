95483b4ef79bd47b528fda4044ee175bfebd309b
wenyngcar <owenyoungcarino@gmail.com>
2025-08-19 16:28:10 +0800

    refactor: separate upload image as component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,13 @@
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import ImproveAccuracy from "./components/ImproveAccuracy";
-import UploadImage from "./components/UploadImage";
+import Form from "./components/form/Form";
 
 function App() {
   return (
     <div className="container p-5 space-y-20 xl:p-7 xl:px-16">
       <Header />
-      <UploadImage />
+      <Form />
       <ImproveAccuracy />
       <Footer />
     </div>
