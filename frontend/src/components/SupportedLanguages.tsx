be471a3be94d160bd89e36e9183339e99523c6f7
wenyngcar <owenyoungcarino@gmail.com>
2025-08-20 19:29:15 +0800

    refactor: reduce supported languages

--- frontend/src/components/SupportedLanguages.tsx
+++ frontend/src/components/SupportedLanguages.tsx
@@ -1,32 +1,22 @@
 import sa from "@/assets/icons/flags/sa.svg";
 import us from "@/assets/icons/flags/us.svg";
 import china from "@/assets/icons/flags/cn.svg";
-import france from "@/assets/icons/flags/fr.svg";
-import german from "@/assets/icons/flags/de.svg";
 import indonesia from "@/assets/icons/flags/id.svg";
-import italy from "@/assets/icons/flags/it.svg";
 import japan from "@/assets/icons/flags/jp.svg";
 import korea from "@/assets/icons/flags/kr.svg";
-import portugal from "@/assets/icons/flags/pt.svg";
 import russia from "@/assets/icons/flags/ru.svg";
-import spain from "@/assets/icons/flags/es.svg";
 import thailand from "@/assets/icons/flags/th.svg";
 import philippines from "@/assets/icons/flags/ph.svg";
 
 const languages = [
-  { flag: us, name: "English" },
   { flag: sa, name: "Arabic" },
   { flag: china, name: "Chinese Simplified" },
   { flag: china, name: "Chinese Traditional" },
-  { flag: france, name: "French" },
-  { flag: german, name: "German" },
+  { flag: us, name: "English" },
   { flag: indonesia, name: "Indonesian" },
-  { flag: italy, name: "Italian" },
   { flag: japan, name: "Japanese" },
   { flag: korea, name: "Korean" },
-  { flag: portugal, name: "Portuguese" },
   { flag: russia, name: "Russian" },
-  { flag: spain, name: "Spanish" },
   { flag: philippines, name: "Tagalog" },
   { flag: thailand, name: "Thai" },
 ];
